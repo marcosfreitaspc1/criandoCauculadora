aa5581b2750a313a55d499d9b265334ed1ee624b
marcosfreitaspc1 <155125047+marcosfreitaspc1@users.noreply.github.com>
2024-01-18 16:54:40 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,76 +1,146 @@
-let monitor = document.getElementById('monitor')
-let res = document.getElementById('res')
-let n0 = document.getElementById('n0')
-let n1 = document.getElementById('n1')
-let n2 = document.getElementById('n2')
-let n3 = document.getElementById('n3')
-let n4 = document.getElementById('n4')
-let n5 = document.getElementById('n5')
-let n6 = document.getElementById('n6')
-let n7 = document.getElementById('n7')
-let n8 = document.getElementById('n8')
-let n9 = document.getElementById('n9')
-let nn = document.getElementById('nn')
-let box = []
-let box1 = ''
-let box2 = []
-let box3 = ''
+var monitor = document.getElementById('monitor')
+var res = document.getElementById('res')
+var n0 = document.getElementById('n0')
+var n1 = document.getElementById('n1')
+var n2 = document.getElementById('n2')
+var n3 = document.getElementById('n3')
+var n4 = document.getElementById('n4')
+var n5 = document.getElementById('n5')
+var n6 = document.getElementById('n6')
+var n7 = document.getElementById('n7')
+var n8 = document.getElementById('n8')
+var n9 = document.getElementById('n9')
+var nn = document.getElementById('nn')
+var i = -1
+var mostrar = ''
+var box1 = ''
+var box2 = ''
+var box3 = []
+var simb1 = ''
+var simb2 = []
+var resultado = ''
 
+//inserir um numero
 function digt1() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '1'
+        box1 = '1'
         exibirNaTela()
     }
 }
 function digt2() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '2'
+        box1 = '2'
         exibirNaTela()
     }
 }
 function digt3() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '3'
+        box1 = '3'
         exibirNaTela()
     }
 }
 function digt4() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '4'
+        box1 = '4'
         exibirNaTela()
     }
 }
 function digt5() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '5'
+        box1 = '5'
         exibirNaTela()
     }
 }
 function digt6() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '6'
+        box1 = '6'
         exibirNaTela()
     }
 }
 function digt7() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '7'
+        box1 = '7'
         exibirNaTela()
     }
 }
 function digt8() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '8'
+        box1 = '8'
         exibirNaTela()
     }
 }
 function digt9() {
     if (box1 == 0 || box1 != 0) {
-        box1 += '9'
+        box1 = '9'
         exibirNaTela()
     }
 }
+
+//exibir o valor inserido e guardalo com number
 function exibirNaTela() {
-    monitor.innerHTML = box1
-    box2[i] = Number(box1)
+    monitor.innerHTML += box1
+    box2 += Number(box1)
+    res.innerHTML = resolver()
+}
+
+//inserir um operador
+function divide() {
+    if (box2 != '') {
+        simb1 = 0
+        contar()
+    }
+}
+function times() {
+    if (box2 != '') {
+        simb1 = 1
+        contar()
+    }
+}
+function minus() {
+    if (box2 != '') {
+        simb1 = 2
+        contar()
+    }
+}
+function plus() {
+    if (box2 != '') {
+        simb1 = 3
+        contar()
+    }
+}
+
+
+function contar() {
+    box3.push(box2)
+    if (simb1 === 0) {
+        monitor.innerHTML += "&divide;"
+    } else if (simb1 == 1) {
+        monitor.innerHTML += "&times;"
+    } else if (simb1 == 2) {
+        monitor.innerHTML += "&minus;"
+    } else if (simb1 == 3) {
+        monitor.innerHTML += "&plus;"
+    }
+    if (resultado === '') {
+        resultado = box2
+    }
+    simb2.push(simb1)
+    box1 = ''
+    box2 = ''
+    i++
+}
+
+//executar uma operação
+
+function resolver() {
+    if (simb2[i] === 0) {
+        resultado = resultado / box2
+    } else if (simb2[i] == 1) {
+        resultado = resultado * box2
+    } else if (simb2[i] == 2) {
+        resultado = resultado - box2
+    } else if (simb2[i] == 3) {
+        resultado = resultado + box2
+    }
+    return resultado
 }
